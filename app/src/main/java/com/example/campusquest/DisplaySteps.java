3fc5a40e3cfe748461afa69592b3335ac957d5e5
Gavin <gavin.fitzgerald@ucdconnect.ie>
2018-10-15 22:36:11 +0100

    Added two activites

---
+++ app/src/main/java/com/example/campusquest/DisplaySteps.java
@@ -0,0 +1,13 @@
+package com.example.campusquest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplaySteps extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.display_steps);
+    }
+}
--- app/src/main/java/com/example/campusquest/MainActivity.java
+++ app/src/main/java/com/example/campusquest/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.campusquest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /** Called when the user taps the Send button */
+    public void countSteps(View view) {
+        Intent intent = new Intent(this, DisplaySteps.class);
+        startActivity(intent);
+    }
+
 }
