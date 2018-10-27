ab060edec2941985dbf2a2aad105f607bca69ba8
maevus <maevus@gmail.com>
2018-10-27 15:32:11 +0100

    Added classes to create database, add data and access database from activites.

--- app/src/main/java/com/example/campusquest/MainActivity.java
+++ app/src/main/java/com/example/campusquest/MainActivity.java
@@ -1,20 +1,31 @@
 package com.example.campusquest;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private CampusQuestOpenHelper mDbOpenHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDbOpenHelper = new CampusQuestOpenHelper(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        mDbOpenHelper.close();
+        super.onDestroy();
     }
 
     /** Called when the user taps the Send button */
     public void countSteps(View view) {
+        // temporary database test
+        SQLiteDatabase sqLiteDatabase = mDbOpenHelper.getReadableDatabase(); // sluggish way of accessing database!
         Intent intent = new Intent(this, DisplaySteps.class);
         startActivity(intent);
     }
