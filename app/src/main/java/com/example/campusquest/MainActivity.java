2b1e8d1e1bf83f6113bf9dd1617b2977e0ae1d35
Gavin <gavin.fitzgerald@ucdconnect.ie>
2018-10-31 12:28:15 +0000

    Record and display steps google fit

--- app/src/main/java/com/example/campusquest/MainActivity.java
+++ app/src/main/java/com/example/campusquest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.campusquest;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,15 +8,17 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     /** Called when the user taps the Send button */
     public void countSteps(View view) {
-        Intent intent = new Intent(this, DisplaySteps.class);
+        Intent intent = new Intent(this, treasure_hunt.class);
         startActivity(intent);
     }
 
