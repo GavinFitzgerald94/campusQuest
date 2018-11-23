2aaf62131c62dfca808107d8da50a1b403833dc9
maevus <maevus@gmail.com>
2018-11-23 17:15:57 +0000

    added radar chart functionality

--- app/src/main/java/com/example/campusquest/CharacterSheet.java
+++ app/src/main/java/com/example/campusquest/CharacterSheet.java
@@ -6,15 +6,30 @@ import android.content.CursorLoader;
 import android.content.Loader;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.RadarChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.RadarData;
+import com.github.mikephil.charting.data.RadarDataSet;
+import com.github.mikephil.charting.data.RadarEntry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.IDataSet;
 import com.mikepenz.materialdrawer.Drawer;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -25,6 +40,7 @@ public class CharacterSheet extends AppCompatActivity implements
         AdapterView.OnItemSelectedListener,
         LoaderManager.LoaderCallbacks<Cursor> {
     public static final int LOADER_STATS = 0;
+    public static final int ATTRIBS = 3;
     private List<String> mAttributes;
     private Spinner mSpinnerAttributes;
     private String mSelectecAttribute;
@@ -35,6 +51,7 @@ public class CharacterSheet extends AppCompatActivity implements
     private int mEndurance;
     private int mIntelligence;
     private Cursor mStatsCursor;
+    private RadarChart mChart;
 
 
     @Override
@@ -57,6 +74,106 @@ public class CharacterSheet extends AppCompatActivity implements
 
         drawer = DrawerUtil.getDrawer(this,toolbar);
 
+        // Create and configure radar chart for displaying stats.
+        mChart = findViewById(R.id.stats_chart);
+        mChart.setBackgroundColor(Color.rgb(60, 65, 82));
+        mChart.getDescription().setEnabled(false);
+        mChart.setWebLineWidth(1f);
+        mChart.setWebColor(R.color.white);
+        mChart.setWebColorInner(R.color.white);
+        mChart.setWebAlpha(100);
+
+        mChart.animateXY(1400, 1400, Easing.EaseInOutQuad, Easing.EaseInOutQuad);
+
+        // Customise x axis
+        XAxis xAxis = mChart.getXAxis();
+        xAxis.setTextSize(9f);
+        xAxis.setYOffset(0);
+        xAxis.setXOffset(0);
+        xAxis.setValueFormatter(new IAxisValueFormatter() {
+
+            // Define attributes
+            private String[] attribs = new String[] {"Strength", "Intelligence", "Endurance"};
+
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                return attribs[(int) value % attribs.length];
+            }
+        });
+
+        xAxis.setTextColor(Color.WHITE);
+
+        // Customise y axis
+        YAxis yAxis  = mChart.getYAxis();
+        yAxis.setLabelCount(ATTRIBS, false);
+        yAxis.setTextSize(9f);
+        // add to define max and min for y
+//        yAxis.setAxisMinimum();
+//        yAxis.setAxisMaximum();
+        yAxis.setDrawLabels(false);
+
+        // Disable chart legend
+        mChart.getLegend().setEnabled(false);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.radar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.refreshValues:
+                setRadarData(); // or call loader again here
+                mChart.invalidate();
+                return true;
+            case R.id.toggleValues:
+                // toggle values on radar chart
+                for (IDataSet<?> set : mChart.getData().getDataSets()) {
+                    set.setDrawValues(!set.isDrawValuesEnabled());
+                }
+                mChart.invalidate();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    /**
+     * Set attribute data to populate attribute radar chart
+     * This function is called after the loader has received all attribute data
+     * from the db cursor.
+     */
+
+    private void setRadarData() {
+        ArrayList<RadarEntry> entries = new ArrayList<>();
+
+        entries.add(new RadarEntry(mEndurance));
+        entries.add(new RadarEntry(mIntelligence));
+        entries.add(new RadarEntry(mStrength));
+
+        RadarDataSet set = new RadarDataSet(entries, "Attributes");
+        set.setColor(Color.GREEN);
+        set.setFillColor(Color.GREEN);
+        set.setDrawFilled(true);
+        set.setFillAlpha(180);
+        set.setLineWidth(2f);
+        set.setDrawHighlightCircleEnabled(true);
+        set.setDrawHighlightIndicators(false);
+
+        // Create radar data object to add to chart
+        RadarData data = new RadarData(set);
+        data.setValueTextSize(9f);
+        data.setDrawValues(true);
+        data.setValueTextColor(Color.WHITE);
+
+        mChart.setData(data);
+        mChart.invalidate();;
     }
 
     private void buildSpinner() {
@@ -131,7 +248,6 @@ public class CharacterSheet extends AppCompatActivity implements
      * Loads player character stats from database
      * @param data
      */
-
     private void loadCharStats (Cursor data) {
         mStatsCursor = data;
 
@@ -147,7 +263,11 @@ public class CharacterSheet extends AppCompatActivity implements
             mEndurance = mStatsCursor.getInt(strPos);
             mIntelligence = mStatsCursor.getInt(endPos);
 
+            // Display stats on graph.
+            setRadarData();
+
         }
+
     }
 
     @Override
