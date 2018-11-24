9da02407c6abcbfe3245712a65f8ac2b6911af2a
maevus <maevus@gmail.com>
2018-11-24 21:42:41 +0000

    removed fab from treasure hunt page

--- app/src/main/java/com/example/campusquest/TreasureHunt.java
+++ app/src/main/java/com/example/campusquest/TreasureHunt.java
@@ -15,8 +15,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -63,8 +61,8 @@ import java.util.concurrent.TimeUnit;
 
 import cn.pedant.SweetAlert.SweetAlertDialog;
 
-import static com.example.campusquest.CampusQuestDatabaseContract.*;
 import static com.example.campusquest.CampusQuestDatabaseContract.CluesInfoEntry;
+import static com.example.campusquest.CampusQuestDatabaseContract.UserCharacterInfoEntry;
 import static com.example.campusquest.CampusQuestDatabaseContract.UserQuestsInfoEntry;
 import static com.example.campusquest.DataManager.getInstance;
 
@@ -129,15 +127,6 @@ public class TreasureHunt extends AppCompatActivity implements
         mTotalStage = bundle.getInt("totalStage");
 
         loadViewContent();
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
 //        mButtonViewToday = (Button) findViewById(R.id.view_today);
 //        //Sets listener for onClick event
